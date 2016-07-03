68e088dc5a3ff2eed447cc2286d7538ecb118fcf
Dang Tung Lam <dangtunglam14@gmail.com>
2016-07-03 21:56:53 +0700

    Add not found page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get '/404' => 'errors#not_found'
+
   root 'documents#new'
+
   get '/' => 'documents#new'
   post '/' => 'documents#create', as: 'document'
   get '/:token' => 'documents#show', :constraints => {:token => /.*/}
+
 end

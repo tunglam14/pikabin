962e77734aec01e705e8be91f9ce3a27132d72b0
Dang Tung Lam <lamdt@vccloud.vn>
2017-04-15 05:06:20 +0000

    Add og tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def page_title(document_title)
+    title = 'PikaBin - Your friendly and secure PasteBin'
+
+    return title if params[:action] == 'new'
+
+    title = if document_title
+      "#{document_title} | #{title}"
+    end
+
+    return title
+  end
 end
